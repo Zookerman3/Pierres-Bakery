8d2c6d01070982ac11312d856b21737f076e075b
Zookerman3 <szook7@gmail.com>
2023-02-25 14:35:56 -0800

    branching halfway done. WIP on refactoring to have the total price of bread to be able to take in more loaves if the user adds more

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -9,37 +9,78 @@ class Program
     {
         Console.WriteLine("Welcome to Pierre's Bakery!");
         Console.WriteLine("Would you like to order Bread or Pastries Today? Please type 'Bread' to order Bread, 'Pastries' to order Pastries or 'both' if you would like to order both");
-       CustomerChoosing();
-    }
-        static void CustomerChoosing()
-        {
-            string userChoice = Console.ReadLine();
-            if (userChoice == "Bread" || userChoice == "bread")
-            {
-                Console.WriteLine("Please enter the amount of bread loaves you would like to order.");
-                Console.WriteLine("Bread loaves are currently buy 2 get one free!");
-                string amountOrdered = Console.ReadLine();
-                int loaves = int.Parse(amountOrdered);
-                Bread newBread = new Bread(loaves);
-                Console.WriteLine("You've Ordered " + newBread.LoavesOrder + " loaves of bread");
-                Console.WriteLine("The total price for your loaves of bread is $" + newBread.TotalPrice);
-                Console.WriteLine("");
-                Console.WriteLine("Would you like to order more? Please type 'Bread' to order Bread, 'Pastries' to order Pastries or 'both' if you would like to order both, or enter 'checkout' to end the transaction");
-                string CustomerOption = Console.ReadLine();
-                if (CustomerOption == "Checkout" || CustomerOption == "checkout");
-                {
-                    Checkout(newBread);
-                }
-            }
-        }
-    static void Checkout(Bread newBread)
-    {
-        Console.WriteLine("Your total checkout price for loaves of bread is: $" + newBread.TotalPrice);
-
-    }
-    static void Chosebread()
-    {
+        CustomerChoosing();
         
     }
-    
+    static void CustomerChoosing()
+    {
+        int loaves = 0;
+        Bread newBread = new Bread(loaves);
+        string userChoice = Console.ReadLine();
+        if (userChoice == "Bread" || userChoice == "bread")
+        {
+            ChooseBread(newBread);
+        }
+        else if
+        (userChoice == "Pastries" || userChoice == "pastries")
+        {
+            ChoosePastry();
+        }
+    }
+    static void ChooseBread(newBread)
+    {
+        Console.WriteLine("Please enter the amount of bread loaves you would like to order.");
+        Console.WriteLine("Bread loaves are currently buy 2 get one free!");
+        string amountOrdered = Console.ReadLine();
+        int loaves = int.Parse(amountOrdered);
+        Bread newBread = new Bread(loaves);
+        Console.WriteLine("You've Ordered " + newBread.LoavesOrder + " loaves of bread");
+        Console.WriteLine("The total price for your loaves of bread is $" + newBread.TotalPrice);
+        Console.WriteLine("");
+        Console.WriteLine("Would you like to order more? Please type 'Bread' to order Bread, 'Pastries' to order Pastries or 'both' if you would like to order both, or enter 'checkout' to end the transaction");
+        string CustomerOption = Console.ReadLine();
+        if (CustomerOption == "Checkout" || CustomerOption == "checkout")
+        {
+            CheckoutBread(newBread);
+        }
+        else if (CustomerOption == "Bread" || CustomerOption == "bread")
+        {
+            ChooseBread();
+        }
+    }
+
+    static void ChoosePastry()
+    {
+        Console.WriteLine("Please enter the amount of pastries you would like to order.");
+        Console.WriteLine("Pastries are currently buy 3 get one free!");
+        string amountOrdered = Console.ReadLine();
+        int pastries = int.Parse(amountOrdered);
+        Pastry newPastry = new Pastry(pastries);
+        Console.WriteLine("You've Ordered " + newPastry.PastryOrder + " pastries");
+        Console.WriteLine("The total price for your pastries is $" + newPastry.PastryTotalPrice);
+        Console.WriteLine("");
+        Console.WriteLine("Would you like to order more? Please type 'Bread' to order Bread, 'Pastries' to order Pastries or 'both' if you would like to order both, or enter 'checkout' to end the transaction");
+        string CustomerOption = Console.ReadLine();
+        if (CustomerOption == "Checkout" || CustomerOption == "checkout")
+        {
+            CheckoutPastry(newPastry);
+        }
+        else if (CustomerOption == "Bread" || CustomerOption == "bread")
+        {
+            ChooseBread();
+        }
+    }
+    static void Checkout(Bread newBread, Pastry newPastry)
+    {
+        Console.WriteLine("Your total checkout price for loaves of bread is: $" + newBread.TotalPrice);
+        Console.WriteLine("Your total checkout price for Pastries is: $" + newPastry.PastryTotalPrice);
+    }
+    static void CheckoutPastry(Pastry newPastry)
+    {
+        Console.WriteLine("Your total checkout price for Pastries is: $" + newPastry.PastryTotalPrice);
+    }
+    static void CheckoutBread(Bread newBread)
+    {
+       Console.WriteLine("Your total checkout price for loaves of bread is: $" + newBread.TotalPrice);
+    }
 }
